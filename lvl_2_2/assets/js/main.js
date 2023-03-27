ed4fa700236846f442ea005ed2a89fe0cb69838c
Rizvan555 <rizvan84@gmx.de>
2023-03-27 16:59:19 +0200

    projects are finished

--- lvl_2_2/assets/js/main.js
+++ lvl_2_2/assets/js/main.js
@@ -4,6 +4,8 @@ function promptGuess() {
   const myGuess = parseInt(prompt("Guess a number between 1 and 10:"));
   if (myGuess === randomNumber) {
     console.log("You are write");
+  } else if (myGuess > 10) {
+    console.log("Please enter between 1 and 10");
   } else {
     console.log("Your guess is wrong");
   }
